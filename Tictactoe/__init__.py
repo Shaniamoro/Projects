00bc5955b612837f80f910ae1ed1275fac5ab669
Shaniamoro <33161479+Shaniamoro@users.noreply.github.com>
2019-10-28 12:55:01 -0400

    Create __init__.py

---
+++ Tictactoe/__init__.py
@@ -0,0 +1,9 @@
+"""
+    Project: Tic-Tac- Toe
+    ~~~~~~~~~~~~~~~~~~~~~
+
+    A simple tic-tac-toe game to play in the CL.
+    The classes are :class:`Game.game`
+"""
+
+__author__ = "Shania Roberts"
